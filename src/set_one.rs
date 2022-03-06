3380ade47f2728a3bcb24675151d2cf65e3d7ac3
Katherine Watson <katie@radicalsemiconductor.com>
2022-03-06 03:56:22 -0800

    Finish 1.2

--- src/set_one.rs
+++ src/set_one.rs
@@ -22,10 +22,31 @@ fn hex_to_base64_exercise() {
 
 fn fixed_xor_exercise() {
     let hex_a = prompt_user("Enter first string: ");
-    let hex_b = prompt_user("Enter second string: ");
+    let a = match hex::decode(&hex_a) {
+        Ok(a) => a,
+        Err(_) => {
+            println!("Invalid first string!");
+            std::process::exit(1);
+        }
+    };
 
-    // if let (Ok(a), Ok(b)) = (hex::decode(hex_a), &hex_b) {
-    // }
+    let hex_b = prompt_user("Enter second string: ");
+    let b = match hex::decode(&hex_b) {
+        Ok(b) => b,
+        Err(_) => {
+            println!("Invalid second string!");
+            std::process::exit(1);
+        }
+    };
+
+    let xor: Vec<u8> = a
+        .iter()
+        .zip(b.iter())
+        .map(|(a, b)| a ^ b)
+        .collect();
+
+    let hex_xor = hex::encode(xor);
+    println!("Result: {}", hex_xor);
 }
 
 fn hex_to_base64(data: &str) -> Result<String, hex::FromHexError> {
