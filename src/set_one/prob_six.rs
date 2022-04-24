089d120cca13430cb87d828f9c94754f0f054973
Katherine Watson <katie@radicalsemiconductor.com>
2022-04-24 00:17:44 -0700

    Move all of 1.6 so far into a function

--- src/set_one/prob_six.rs
+++ src/set_one/prob_six.rs
@@ -1,7 +1,10 @@
 use std::fs;
+use std::str;
 
 use crate::prompt::prompt_user;
-use crate::set_one::xor::{detect_xor_keysizes, decode_single_byte_xor};
+use crate::set_one::dist::hamming_dist;
+use crate::set_one::xor::{detect_xor_keysizes, decode_single_byte_xor,
+    decrypt_repeating_xor, DetectXORSettings};
 
 pub fn break_repeating_key_xor_exercise() {
     let filename = prompt_user("Enter ciphertext filename: ");
@@ -11,20 +14,37 @@ pub fn break_repeating_key_xor_exercise() {
         .collect();
     let ct = base64::decode(ct_base64)
         .expect("Invalid base64 encoding!");
-    let sizes = detect_xor_keysizes(&ct);
 
-    for &size in sizes[0..2].iter() {
+    let ds = DetectXORSettings {
+        pairs: 10,
+        maxlen: 40
+    };
+
+    let sizes = detect_xor_keysizes(&ct, 40, 10);
+
+
+    // for &size in sizes[0..5].iter() {
+    for &size in sizes.iter() {
         let chunks: Vec<&[u8]> = ct.chunks(size).collect();
-        let maybe_key: Vec<u8> = (0..size)
+        let maybe_key: Option<Vec<u8>> = (0..size)
             .map(|i| {
                 let ct: Vec<u8> = chunks
                     .iter()
                     .filter_map(move |chunk| chunk.get(i))
-                    .map(|&x| x)
+                    .copied()
                     .collect();
                 decode_single_byte_xor(&ct).map(|d| d.key)
-            }).flatten().collect();
+            }).collect();
 
+        if let Some(key) = maybe_key {
+            let pt = decrypt_repeating_xor(&ct, &key);
+            println!("{}", size);
+            println!("{:?}", str::from_utf8(&key).unwrap());
+            println!("{:?}", str::from_utf8(&pt));
+            println!("{}", hamming_dist(&pt[0..size], &pt[size..2*size]));
+            println!("\n\n\n\n");
+        }
     }
+
     println!("Key size: {:?}", sizes);
 }
--- src/set_one/xor.rs
+++ src/set_one/xor.rs
@@ -3,11 +3,26 @@ use std::str::from_utf8;
 use crate::set_one::freq::score_freqs;
 use crate::set_one::dist::hamming_dist;
 
+
 pub struct SingleByteXORDecode {
     pub pt: String,
     pub key: u8,
     pub score: f32
 }
+
+
+pub struct RepeatingXORDecode {
+    pub pt: String,
+    pub key: Vec<u8>,
+    pub score: f32
+}
+
+
+pub struct DetectXORSettings {
+    pairs: usize,
+    maxlen: usize
+}
+
 
 pub fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
     a.iter().zip(b.iter())
@@ -63,22 +78,63 @@ pub fn encrypt_repeating_xor(pt: &[u8], key: &[u8]) -> Vec<u8> {
         .collect()
 }
 
-pub fn decrypt_repeating_xor(pt: &[u8], key: &[u8]) -> Vec<u8> {
-    encrypt_repeating_xor(pt, key)
+pub fn decrypt_repeating_xor(ct: &[u8], key: &[u8]) -> Vec<u8> {
+    encrypt_repeating_xor(ct, key)
 }
 
 
-pub fn detect_xor_keysizes(ct: &[u8]) -> Vec<usize>{
-    let mut dist_pairs: Vec<(usize, u32)> = (2..40).map(|size| {
-        let dist = hamming_dist(&ct[0..size], &ct[size..2*size]);
-        (size, dist)
+pub fn detect_xor_keysizes(ct: &[u8], ds: &DetectXORSettings) -> Vec<usize>{
+    let mut dist_pairs: Vec<(usize, f32)> = (2..ds.maxlen).map(|size| {
+        let dist: u32 = (0..ds.pairs).map(|i| hamming_dist(
+                &ct[i*size..(i + 1)*size],
+                &ct[(i + 1)*size..(i + 2)*size]
+            )).sum();
+        (size, dist as f32 / (3 * size) as f32)
     }).collect();
 
-    dist_pairs.sort_by(|&(s1, d1), &(s2, d2)| {
-        let r1 = d1 as f32 / s1 as f32;
-        let r2 = d2 as f32 / s2 as f32;
-        r1.partial_cmp(&r2).unwrap()
+    dist_pairs.sort_by(|&(_ , s1), &(_, s2)| {
+        s1.partial_cmp(&s2).unwrap()
     });
 
     dist_pairs.into_iter().map(|(size, _)| size).collect()
+}
+
+pub fn decode_repeating_xor(ct: &[u8], ds: &DetectXORSettings) -> Option<RepeatingXORDecode> {
+    let sizes = detect_xor_keysizes(ct, ds);
+
+    let pt_key_pairs = sizes.into_iter().filter_map(|size| {
+        let chunks: Vec<&[u8]> = ct.chunks(size).collect();
+
+        let maybe_key: Option<Vec<u8>> = (0..size)
+            .map(|i| {
+                let ct: Vec<u8> = chunks
+                    .iter()
+                    .filter_map(move |chunk| chunk.get(i))
+                    .copied()
+                    .collect();
+                decode_single_byte_xor(&ct).map(|d| d.key)
+            }).collect();
+
+        let maybe_pair = maybe_key.and_then(|key| {
+            let pt_bytes: Vec<u8> = decrypt_repeating_xor(&ct, &key);
+            
+            let maybe_pt: Option<String> = from_utf8(&pt_bytes)
+                .ok()
+                .map(|text| text.to_owned());
+
+            maybe_pt.map(|pt| (pt, key))
+        });
+
+        maybe_pair
+    });
+
+    let decodes = pt_key_pairs
+        .map(|(pt, key)| {
+            let score = score_freqs(&pt);
+            RepeatingXORDecode {pt, key, score}
+        });
+
+    decodes.min_by(|d1, d2| {
+        d1.score.partial_cmp(&d2.score).unwrap()
+    })
 }
